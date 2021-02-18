234ef9f76948447a799b7fc394cfb86d70a0ca47
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-18 17:31:42 +0100

    Update exercice1.py

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,2 +1,9 @@
-def taille(arbre, lettre):
+def convertir(T):
+    """
+    T est un tableau d'entiers, dont les éléments sont 0 ou 1 et
+    représentant un entier écrit en binaire. Renvoie l'écriture
+    décimale de l'entier positif dont la représentation binaire
+    est donnée par le tableau T
+    """
+    ...
     return ...
