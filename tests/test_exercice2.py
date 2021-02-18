c9af08e7c8c39afad045c60a1a33457b499ce05e
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-18 17:39:49 +0100

    Update test_exercice2.py

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,4 +1,5 @@
 from exercices.exercice2 import *
 
-def test_tri_iteratif():
-    assert tri_iteratif([41, 55, 21, 18, 12, 6, 25]) == [6, 12, 18, 21, 25, 41, 55]
+def test_assert tri_insertion():
+    assert tri_insertion([2, 5, -1, 7, 0, 28]) == [-1, 0, 2, 5, 7, 28]
+    assert tri_insertion([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
